b189a9138b53592852ef38aa01f27dc484cd8723
Todd Austin <austin.todd.j@gmail.com>
2017-12-02 08:07:49 -0600

    Analytics tracking and asset cleanup

--- resources/views/public/index.blade.php
+++ resources/views/public/index.blade.php
@@ -1,99 +1,14 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.public')
 
-    <title>{{ env('APP_NAME') }}</title>
-
-    <!-- Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-
-    <!-- Styles -->
-    <style>
-        html, body {
-            background-color: #fff;
-            color: #636b6f;
-            font-family: 'Raleway', sans-serif;
-            font-weight: 100;
-            height: 100vh;
-            margin: 0;
-        }
-
-        .full-height {
-            height: 100vh;
-        }
-
-        .flex-center {
-            align-items: center;
-            display: flex;
-            justify-content: center;
-        }
-
-        .position-ref {
-            position: relative;
-        }
-
-        .top-right {
-            position: absolute;
-            right: 10px;
-            top: 18px;
-        }
-
-        .content {
-            text-align: center;
-        }
-
-        .title {
-            font-size: 84px;
-        }
-
-        .links > a {
-            color: #636b6f;
-            padding: 0 25px;
-            font-size: 12px;
-            font-weight: 600;
-            letter-spacing: .1rem;
-            text-decoration: none;
-            text-transform: uppercase;
-        }
-
-        .m-b-md {
-            margin-bottom: 30px;
-        }
-
-        img {
-            width: 316px;
-        }
-    </style>
-</head>
-<body>
-<div class="flex-center position-ref full-height">
-    @if (Route::has('login'))
-        <div class="top-right links">
-            @auth
-                <a href="{{ url('/dashboard') }}">Dashboard</a>
-            @else
-                <a href="{{ route('login') }}">Login</a>
-                @if (config('auth.registration'))
-                    <a href="{{ route('register') }}">Register</a>
-                @endif
-            @endauth
-        </div>
-    @endif
-
+@section('content')
     <div class="content">
         <div class="title m-b-md">
             <img src="{{ asset('img/laravel-zoom.png') }}">
         </div>
-
         <div class="links">
             <a href="https://github.com/austintoddj/laravel-zoom#features" target="_blank">Features</a>
             <a href="https://github.com/austintoddj/laravel-zoom" target="_blank">GitHub</a>
             <a href="https://github.com/austintoddj/laravel-zoom#configuration" target="_blank">Configuration</a>
         </div>
     </div>
-</div>
-</body>
-</html>
+@endsection
