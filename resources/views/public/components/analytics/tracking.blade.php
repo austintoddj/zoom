6c84408e4584f322a446de24e514aaa6dffe278c
Todd Austin <austin.todd.j@gmail.com>
2017-12-02 08:11:30 -0600

    Fixed tracking code bug

--- resources/views/public/components/analytics/tracking.blade.php
+++ resources/views/public/components/analytics/tracking.blade.php
@@ -4,6 +4,6 @@
         m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
     })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-    ga('create', {{ env('GOOGLE_ANALYTICS') }}, 'auto');
+    ga('create', '{{ env("GOOGLE_ANALYTICS") }}', 'auto');
     ga('send', 'pageview');
 </script>
