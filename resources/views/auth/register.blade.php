e3e235b68631125e587fddfa19924d95bf797283
Todd Austin <austin.todd.j@gmail.com>
2017-12-13 14:08:47 -0600

    Updates to the login and registration forms

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -9,13 +9,16 @@
             <form role="form" method="POST" action="{{ url('/register') }}">
                 {!! csrf_field() !!}
 
-                <div class="form-group row">
-                    <label class="col-lg-4 col-form-label text-lg-right">Name</label>
-                    <div class="col-lg-6">
+                <div class="col-lg-8 offset-lg-2">
+                    <div class="form-group">
                         @if(!empty($name))
-                            <input id="name" type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name" value="{{ $name }}" required autofocus>
+                            <input id="name" type="text"
+                                   class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name"
+                                   value="{{ $name }}" placeholder="Name" required autofocus>
                         @else
-                            <input id="name" type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name" value="{{ old('name') }}" required autofocus>
+                            <input id="name" type="text"
+                                   class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name"
+                                   value="{{ old('name') }}" placeholder="Name" required autofocus>
                         @endif
                         @if ($errors->has('name'))
                             <div class="invalid-feedback">
@@ -23,15 +26,16 @@
                             </div>
                         @endif
                     </div>
-                </div>
 
-                <div class="form-group row">
-                    <label for="email" class="col-lg-4 col-form-label text-lg-right">E-Mail Address</label>
-                    <div class="col-md-6">
+                    <div class="form-group">
                         @if(!empty($email))
-                            <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ $email }}" required>
+                            <input id="email" type="email"
+                                   class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email"
+                                   value="{{ $email }}" placeholder="E-Mail Address" required>
                         @else
-                            <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required>
+                            <input id="email" type="email"
+                                   class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email"
+                                   placeholder="E-Mail Address" value="{{ old('email') }}" required>
                         @endif
                         @if ($errors->has('email'))
                             <div class="invalid-feedback">
@@ -39,55 +43,40 @@
                             </div>
                         @endif
                     </div>
-                </div>
 
-                <div class="form-group row">
-                    <label class="col-lg-4 col-form-label text-lg-right">Password</label>
-
-                    <div class="col-lg-6">
-                        <input
-                                type="password"
-                                class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}"
-                                name="password"
-                                required
-                        >
+                    <div class="form-group">
+                        <input type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}"
+                               name="password" placeholder="Password" required>
                         @if ($errors->has('password'))
                             <div class="invalid-feedback">
                                 <strong>{{ $errors->first('password') }}</strong>
                             </div>
                         @endif
                     </div>
-                </div>
 
-                <div class="form-group row">
-                    <label class="col-lg-4 col-form-label text-lg-right">Confirm Password</label>
-
-                    <div class="col-lg-6">
-                        <input
-                                type="password"
-                                class="form-control{{ $errors->has('password_confirmation') ? ' is-invalid' : '' }}"
-                                name="password_confirmation"
-                                required
-                        >
+                    <div class="form-group">
+                        <input type="password"
+                               class="form-control{{ $errors->has('password_confirmation') ? ' is-invalid' : '' }}"
+                               name="password_confirmation" placeholder="Confirm Password" required>
                         @if ($errors->has('password_confirmation'))
                             <div class="invalid-feedback">
                                 <strong>{{ $errors->first('password_confirmation') }}</strong>
                             </div>
                         @endif
                     </div>
-                </div>
 
-                <div class="form-group row">
-                    <div class="col-lg-6 offset-lg-4">
-                        <button type="submit" class="btn btn-primary">
+                    <div class="form-group">
+                        <button type="submit" class="btn btn-primary btn-block">
                             Register
                         </button>
                     </div>
-                </div>
 
-                @if(env('SOCIALITE'))
-                    @include('auth.components.socialite.links')
-                @endif
+                    <div class="form-group">
+                        @if(env('SOCIALITE'))
+                            @include('auth.components.socialite.links')
+                        @endif
+                    </div>
+                </div>
             </form>
         </div>
     </div>
