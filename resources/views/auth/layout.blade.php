2b2b4b9ecab66182c283bedb07684ebe8aad5664
Todd Austin <austin.todd.j@gmail.com>
2018-09-20 09:35:55 -0500

    Asset updates

--- resources/views/auth/layout.blade.php
+++ resources/views/auth/layout.blade.php
@@ -8,11 +8,6 @@
     <script defer src="{{ url('https://use.fontawesome.com/releases/v5.3.1/js/all.js') }}" integrity="sha384-kW+oWsYx3YpxvjtZjFXqazFpA7UP/MbiY4jvs+RWZo2+N94PFZ36T6TFkc9O3qoB" crossorigin="anonymous"></script>
 @endpush
 
-@push('fonts')
-    <link rel="dns-prefetch" href="{{ url('https://fonts.gstatic.com') }}">
-    <link href="{{ url('https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,800,800i,900,900i') }}}" rel="stylesheet">
-@endpush
-
 @push('styles')
     <link href="{{ asset('css/auth.css') }}" rel="stylesheet">
 @endpush
--- resources/views/public/layout.blade.php
+++ resources/views/public/layout.blade.php
@@ -6,11 +6,6 @@
     @endif
 @endpush
 
-@push('fonts')
-    <link rel="dns-prefetch" href="{{ url('https://fonts.gstatic.com') }}">
-    <link href="{{ url('https://fonts.googleapis.com/css?family=Nunito:200,600') }}" rel="stylesheet" type="text/css">
-@endpush
-
 @push('styles')
     <link href="{{ asset('css/public.css') }}" rel="stylesheet">
 @endpush
