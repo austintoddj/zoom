516831408c14dc1d518938021f9b883a92161d20
Todd Austin <austin.todd.j@gmail.com>
2018-09-19 20:05:22 -0500

    Cleanup

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -20,13 +20,13 @@ class Kernel extends ConsoleKernel
     /**
      * Define the application's command schedule.
      *
-     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
+     * @param  \Illuminate\Console\Scheduling\Schedule $schedule
      * @return void
      */
     protected function schedule(Schedule $schedule)
     {
-				// $schedule->command('backup:clean')->daily()->at('01:00');
-   			// $schedule->command('backup:run')->daily()->at('02:00');
+//        $schedule->command('backup:clean')->daily()->at('01:00');
+//        $schedule->command('backup:run')->daily()->at('02:00');
     }
 
     /**
@@ -36,7 +36,7 @@ class Kernel extends ConsoleKernel
      */
     protected function commands()
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
