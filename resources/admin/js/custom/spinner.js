9bfaf90d7fd8223a63759197094cfe7ecb54cf85
Todd Austin <austin.todd.j@gmail.com>
2018-10-31 08:59:40 -0500

    Updated the assets

--- resources/admin/js/custom/spinner.js
+++ resources/admin/js/custom/spinner.js
@@ -2,7 +2,11 @@ $(function() {
     let button = $('.btn-spin');
 
     button.on('click', function () {
-        button.addClass('disabled');
-        button.html("<i class='fas fa-fw fa-circle-notch fa-spin'></i> " + button.text());
+        let form = $(this).closest('form');
+
+        if (form[0].checkValidity()) {
+            button.addClass('disabled');
+            button.html("<i class='fas fa-fw fa-circle-notch fa-spin'></i> " + button.text());
+        }
     });
 });
