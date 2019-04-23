6b35c4ac64ef63ea5844b8aebd117fe8f874e25d
Todd Austin <austin.todd.j@gmail.com>
2019-04-23 08:43:41 -0500

    Apply fixes from StyleCI (#52)

--- app/Jobs/UpdateSettings.php
+++ app/Jobs/UpdateSettings.php
@@ -6,9 +6,10 @@ use App\User;
 use Carbon\Carbon;
 use App\Helpers\PhoneNumber;
 use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
-use Illuminate\Queue\{SerializesModels, InteractsWithQueue};
 
 class UpdateSettings implements ShouldQueue
 {
