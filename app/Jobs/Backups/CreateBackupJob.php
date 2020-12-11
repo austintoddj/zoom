5baee892d5c9bc4163eec9d50ccb9c25c982472a
Todd Austin <austin.todd.j@gmail.com>
2020-12-11 04:27:15 -0600

    Apply fixes from StyleCI (#72)

--- app/Jobs/Backups/CreateBackupJob.php
+++ app/Jobs/Backups/CreateBackupJob.php
@@ -4,10 +4,10 @@ namespace App\Jobs\Backups;
 
 use Exception;
 use Illuminate\Bus\Queueable;
-use Illuminate\Queue\SerializesModels;
-use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
 use Spatie\Backup\Tasks\Backup\BackupJobFactory;
 
 class CreateBackupJob implements ShouldQueue
