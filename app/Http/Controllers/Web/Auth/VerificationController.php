0d20b3cb84e9c0cc91f59d8b8d47bb15ae603d69
Todd Austin <austintoddj@users.noreply.github.com>
2021-05-10 17:36:11 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- app/Http/Controllers/Web/Auth/VerificationController.php
+++ app/Http/Controllers/Web/Auth/VerificationController.php
@@ -2,8 +2,8 @@
 
 namespace App\Http\Controllers\Web\Auth;
 
-use Illuminate\Routing\Controller;
 use Illuminate\Foundation\Auth\VerifiesEmails;
+use Illuminate\Routing\Controller;
 
 class VerificationController extends Controller
 {
